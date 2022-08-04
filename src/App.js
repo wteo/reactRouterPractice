ad7ef6c7725f64b414054d23d7b0545266bba14d
Wendy Teo <wendyteo.wy@gmail.com>
2022-08-05 06:58:32 +1000

    Added Lazy Loading to About component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 
 import Header from './pages/Header';
 import Welcome from './pages/Welcome';
-import About from './pages/About';
+// import About from './pages/About';
 import Contact from './pages/Contact';
 import PageNotFound from './pages/PageNotFound';
+import Overlay from './ui/Overlay';
 
-import AboutEducation from './pages/about/About_Education';
-import AboutExperiences from './pages/about/About_Experiences';
-import AboutProjects from './pages/about/About_Projects';
+const About = React.lazy(() => import('./pages/About'));
+
+const AboutEducation = React.lazy(() => import('./pages/about/About_Education'));
+const AboutExperiences = React.lazy(() => import('./pages/about/About_Experiences'));
+const AboutProjects = React.lazy(() => import('./pages/about/About_Projects'));
+
+// import AboutEducation from './pages/about/About_Education';
+// import AboutExperiences from './pages/about/About_Experiences';
+// import AboutProjects from './pages/about/About_Projects';
 
 export const url = {
   about: "about",
@@ -22,17 +29,19 @@ function App() {
   return (
     <React.Fragment>
       <Header />
-      <Routes>
-        <Route path='/' element={<Navigate replace to='/welcome'/>} />
-        <Route path={`/${url.welcome}`} element={<Welcome />} />
-        <Route path={`/${url.about}/`} element={<About/>}>
-            <Route path='education' element={<AboutEducation />} />
-            <Route path='experience' element={<AboutExperiences />} />
-            <Route path='projects' element={<AboutProjects />} />
-        </Route>
-        <Route path={`/${url.contact}`} element={<Contact/>} />
-        <Route path='*' element={<PageNotFound/>} />
-      </Routes>
+      <Suspense fallback={<Overlay><p>Loading...</p></Overlay>}>
+        <Routes>
+          <Route path='/' element={<Navigate replace to='/welcome'/>} />
+          <Route path={`/${url.welcome}`} element={<Welcome />} />
+          <Route path={`/${url.about}/`} element={<About/>}>
+              <Route path='education' element={<AboutEducation />} />
+              <Route path='experience' element={<AboutExperiences />} />
+              <Route path='projects' element={<AboutProjects />} />
+          </Route>
+          <Route path={`/${url.contact}`} element={<Contact/>} />
+          <Route path='*' element={<PageNotFound/>} />
+        </Routes>
+      </Suspense>
     </React.Fragment>
   );
 }
